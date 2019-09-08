945e0f949209479861d53b0e34bf0ffa4bdc1db3
lx <sa514075@mail.ustc.edu.cn>
2019-09-08 22:06:31 +0800

    update

--- src/reverse-str/index.js
+++ src/reverse-str/index.js
@@ -2,8 +2,8 @@ const genBigArr = require('../utils/generate-big-array');
 const reverse = require('../utils/reverse');
 const Timer = require('../utils/timer');
 
-const n = 100000; // 总长度
-const x = 100; // 切分点
+const n = 1000000; // 总长度
+const x = 6554; // 切分点
 
 const timer = new Timer();
 const arr = genBigArr(n);
@@ -22,9 +22,20 @@ const normalMove = (rawArr, x) => {
     arr[length - 1] = temp;
   }
   timer.stop().print();
+  return arr;
+}
+
+// js slice
+const sliceMove = (rawArr, x) => {
+  timer.start();
+  const arr = Array.from(rawArr);
+  const sliceArr = arr.splice(0, x);
+  timer.stop().print();
+  return arr.concat(sliceArr);
 }
 
 // 反转算法
+// 将数组从x处分开，分为ab两部分，a反转，b反转，再对整个数组反转
 const reverseMove = (rawArr, x) => {
   timer.start();
   let arr = Array.from(rawArr);
@@ -35,7 +46,9 @@ const reverseMove = (rawArr, x) => {
   // 整体反转
   reverse(arr, 0, arr.length - 1);
   timer.stop().print();
+  return arr;
 }
 
-normalMove(arr, n, x);
+normalMove(arr, x);
+sliceMove(arr, x);
 reverseMove(arr, x);
